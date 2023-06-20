370b5487be0a8e3abdd4299faae1c93f339171c9
Viet Dang <vietdanghoang1705@gmail.com>
2023-06-20 23:07:59 +0700

    fix route

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const db = require("./config/dbConfig");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+app.use(cors())
 db.connect();
 routerMain(app);
 
